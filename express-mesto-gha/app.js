acb2ab6c3f7acb8e847388a5bd1f559a820e04c8
ann syharevskaya <annsyharevskaya@yandex.ru>
2022-05-15 17:52:24 +0000

    add auth to routs and dell it from App.js

--- express-mesto-gha/app.js
+++ express-mesto-gha/app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const BodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const auth = require('./middlewares/auth'); // авторизация
 const cenralErrors = require('./middlewares/central-err');
 
 const app = express();
@@ -51,7 +50,6 @@ app.use(cors);
 
 
 app.use(BodyParser.json()); // подключили миддлвару кот достает значения из тела запроса
-app.use(auth); // авторизация
 
 // подключаем роуты и всё остальное...
 app.use(express.json());
